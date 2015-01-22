26c27dadab27dcba35fce79aa3f4b10b59664f8a
Marc Lee & David Zulaica <student@epicodus.com>
2015-01-22 10:44:19 -0800

    Add initial spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('pg')
+require('expense')
+require('category')
+
+DB = PG.connect({:dbname => "expense_organizer_test"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM expenses *;")
+    DB.exec("DELETE FROM categories *;")
+  end
+end
